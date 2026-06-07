a28ddaff964405199825bece2c26831b37442cb6
Mahish Praveen <mahi.devex@gmail.com>
2026-06-07 15:50:49 +0530

    Update ArithmeticOperations.py

--- Arithmetic/ArithmeticOperations.py
+++ Arithmetic/ArithmeticOperations.py
@@ -1,1 +1,26 @@
-
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+choice=input("Enter the choice +-*///**%")
+if choice=="+":
+    result=a+b
+    print("sum=",result)
+elif choice="-":
+    result=a-b
+    print(result)
+elif choice="*":
+    result=a*b
+    print(result)
+elif choice="/":
+    result=a/b
+    print(result)
+elif choice ="//":
+    result=a//b
+    print(result)
+elif choice ="**":
+    result=a**b
+    print(result)
+elif choice ="%":
+    result=a%b
+    print(result)
+else:
+    print("Invalid Operator")
