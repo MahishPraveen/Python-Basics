89f87b8a9f577273c5b7f9fba27970b774652cb3
Mahish Praveen <mahi.devex@gmail.com>
2026-06-07 15:44:52 +0530

    Add files via upload

---
+++ Arithmetic/BatsmanScore.py
@@ -0,0 +1,11 @@
+runs=int(input("Enter the score"))
+if runs>=600:
+    print("man of the series")
+elif runs>=500:
+    print("superlevel")
+elif runs>=400:
+    print("good batsman")
+elif runs>=300:
+    print("average batsman")
+else:
+    print("Can do better")
