684ab2e2bd4495872e2642047c33c3970b4eda06
Mahish Praveen <mahi.devex@gmail.com>
2026-06-07 15:46:23 +0530

    Update Find_Character.py

--- Strings/Find_Character.py
+++ Strings/Find_Character.py
@@ -1,1 +1,10 @@
+ch=input("Enter a character")
+if ch>="A" and ch<="Z":
+    print("It is uppercase character")
+elif ch>="a" and ch<="z":
+    print("It is lowercase character")
+elif ch>="0" and ch<="9":
+    print("It is a digit")
+else:
+    print("It is a special character")
 
