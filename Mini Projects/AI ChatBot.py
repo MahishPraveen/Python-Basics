503bf49ccabd82c0dac108f84441144bacc09a7a
Mahish Praveen <mahi.devex@gmail.com>
2026-06-07 16:06:42 +0530

    Add files via upload

---
+++ Mini Projects/AI ChatBot.py
@@ -0,0 +1,15 @@
+#AI Chat Bot
+
+while True:
+    user=input("You: ").lower()
+    if user=="hello":
+        print("Bot: Hello there!")
+    elif user=="How are you":
+        print("Bot: I am doing great!")
+    elif user=="Who created you":
+        print("Bot: I was created by Mahish")
+    elif user=="Thanks":
+        print("Bot: Your Welcome")
+    elif user=="Bye":
+        print("bot: Goodbye!")
+    
